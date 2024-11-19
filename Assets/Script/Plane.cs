2870f479aa126a7b60f287500d2dae81ccee2495
zkdlfl <159306744+zkdlfl@users.noreply.github.com>
2024-11-19 14:27:32 -0500

    Synced Text to Variables

--- Assets/Script/Plane.cs
+++ Assets/Script/Plane.cs
@@ -1,29 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Player_Plane{
-    public string PlayerName;
     public string Color;
+    public int numPlanes;
+    public int kills;
     public bool [] finalSpace; // checks which home bases are taken
     public int [][] coordinates; // finds the (x, y) coordinates of the four planes
-    public Player_Plane(string PlayerName, string color){
-        this.PlayerName = PlayerName;
+    public Player_Plane(int numPlanes, string color, int kill){
+        this.numPlanes = numPlanes;
+        this.kills = kill;
         this.Color = color;
     }
 }
 public class Plane : MonoBehaviour
 {
-    
+    // Text
+    public TextMeshProUGUI yellow_Text;
+    public TextMeshProUGUI red_Text;
+    public TextMeshProUGUI blue_Text;
+    public TextMeshProUGUI green_Text;
+    public TextMeshProUGUI player_turn_text;
+
+    public string PlayerTurn;
+    Player_Plane yellow_profile = new Player_Plane(4, "Yellow", 0);
+    Player_Plane red_profile = new Player_Plane(4, "Red", 0);
+    Player_Plane blue_profile = new Player_Plane(4, "Blue", 0);
+    Player_Plane green_profile = new Player_Plane(4, "Green", 0);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerTurn = "Yellow";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        player_turn_text.text = PlayerTurn;
+        yellow_Text.text = "Planes: " + yellow_profile.numPlanes.ToString() + ", Kills: " + yellow_profile.kills.ToString();
+        red_Text.text = "Planes: " + red_profile.numPlanes.ToString() + ", Kills: " + red_profile.kills.ToString();
+        blue_Text.text = "Planes: " + blue_profile.numPlanes.ToString() + ", Kills: " + blue_profile.kills.ToString();
+        green_Text.text = "Planes: " + green_profile.numPlanes.ToString() + ", Kills: " + green_profile.kills.ToString();
     }
 }
