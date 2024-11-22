b311f9ed3896b062e3441b6a0bea5bc49df15a1f
acise314 <puyuhuan1@gmail.com>
2024-11-22 11:35:07 -0500

    Base coordinates

--- Assets/Script/Coordinates.cs
+++ Assets/Script/Coordinates.cs
@@ -44,117 +44,119 @@ public class Coordinate : MonoBehaviour
     {
         list_coordinates = new List<xy>();
         base_coordinates = new List<xy>();
-        var batch = new List<xy>
-        {
-            new xy(220.00, 352.00),
-            new xy(247.00, 361.00),
-            new xy(273.00, 360.00),
-            new xy(301.00, 350.00),
-            new xy(319.00, 372.00),
-            new xy(309.00, 401.00),
-            new xy(308.00, 428.00),
-            new xy(320.00, 455.00),
-            new xy(346.00, 466.00),
-            new xy(372.00, 466.00),
-            new xy(397.00, 463.00),
-            new xy(423.00, 467.00),
-            new xy(447.00, 466.00),
-            new xy(474.00, 455.00),
-            new xy(484.00, 429.00),
-            new xy(484.00, 402.00),
-            new xy(474.00, 371.00),
-            new xy(492.00, 352.00),
-            new xy(521.00, 361.00),
-            new xy(546.00, 362.00),
-            new xy(572.00, 350.00),
-            new xy(581.00, 322.00),
-            new xy(581.00, 295.00),
-            new xy(581.00, 268.00),
-            new xy(580.00, 241.00),
-            new xy(581.00, 216.00),
-            new xy(572.00, 186.00),
-            new xy(545.00, 175.00),
-            new xy(520.00, 177.00),
-            new xy(493.00, 185.00),
-            new xy(474.00, 166.00),
-            new xy(484.00, 135.00),
-            new xy(483.00, 109.00),
-            new xy(474.00, 83.00),
-            new xy(447.00, 71.00),
-            new xy(422.00, 71.00),
-            new xy(396.00, 74.00),
-            new xy(371.00, 72.00),
-            new xy(346.00, 71.00),
-            new xy(319.00, 82.00),
-            new xy(310.00, 110.00),
-            new xy(310.00, 135.00),
-            new xy(321.00, 166.00),
-            new xy(300.00, 187.00),
-            new xy(272.00, 177.00),
-            new xy(247.00, 177.00),
-            new xy(220.00, 189.00),
-            new xy(213.00, 216.00),
-            new xy(213.00, 243.00),
-            new xy(213.00, 268.00),
-            new xy(212.00, 295.00),
-            new xy(213.00, 321.00)
-        };
-        var group = new List<xy>
-        {
-            new xy(203.00, 369.00), // 1st one of the list is the first step outside the hanger
-            new xy(211.00, 461.00), // HOME BASE
-            new xy(260.00, 459.00),
-            new xy(210.00, 412.00),
-            new xy(260.00, 413.00),
-            new xy(257.00, 268.00), // RUNWAY TO LEAVE
-            new xy(281.00, 268.00),
-            new xy(304.00, 268.00),
-            new xy(325.00, 268.00),
-            new xy(346.00, 267.00),
-            new xy(369.00, 269.00), //
+        /*     var batch = new List<xy>
+             {
+                 new xy(220.00, 352.00),
+                 new xy(247.00, 361.00),
+                 new xy(273.00, 360.00),
+                 new xy(301.00, 350.00),
+                 new xy(319.00, 372.00),
+                 new xy(309.00, 401.00),
+                 new xy(308.00, 428.00),
+                 new xy(320.00, 455.00),
+                 new xy(346.00, 466.00),
+                 new xy(372.00, 466.00),
+                 new xy(397.00, 463.00),
+                 new xy(423.00, 467.00),
+                 new xy(447.00, 466.00),
+                 new xy(474.00, 455.00),
+                 new xy(484.00, 429.00),
+                 new xy(484.00, 402.00),
+                 new xy(474.00, 371.00),
+                 new xy(492.00, 352.00),
+                 new xy(521.00, 361.00),
+                 new xy(546.00, 362.00),
+                 new xy(572.00, 350.00),
+                 new xy(581.00, 322.00),
+                 new xy(581.00, 295.00),
+                 new xy(581.00, 268.00),
+                 new xy(580.00, 241.00),
+                 new xy(581.00, 216.00),
+                 new xy(572.00, 186.00),
+                 new xy(545.00, 175.00),
+                 new xy(520.00, 177.00),
+                 new xy(493.00, 185.00),
+                 new xy(474.00, 166.00),
+                 new xy(484.00, 135.00),
+                 new xy(483.00, 109.00),
+                 new xy(474.00, 83.00),
+                 new xy(447.00, 71.00),
+                 new xy(422.00, 71.00),
+                 new xy(396.00, 74.00),
+                 new xy(371.00, 72.00),
+                 new xy(346.00, 71.00),
+                 new xy(319.00, 82.00),
+                 new xy(310.00, 110.00),
+                 new xy(310.00, 135.00),
+                 new xy(321.00, 166.00),
+                 new xy(300.00, 187.00),
+                 new xy(272.00, 177.00),
+                 new xy(247.00, 177.00),
+                 new xy(220.00, 189.00),
+                 new xy(213.00, 216.00),
+                 new xy(213.00, 243.00),
+                 new xy(213.00, 268.00),
+                 new xy(212.00, 295.00),
+                 new xy(213.00, 321.00)
+             };
+             var group = new List<xy>
+             {
+                 new xy(203.00, 369.00), // 1st one of the list is the first step outside the hanger
+                 new xy(211.00, 461.00), // HOME BASE
+                 new xy(260.00, 459.00),
+                 new xy(210.00, 412.00),
+                 new xy(260.00, 413.00),
+                 new xy(257.00, 268.00), // RUNWAY TO LEAVE
+                 new xy(281.00, 268.00),
+                 new xy(304.00, 268.00),
+                 new xy(325.00, 268.00),
+                 new xy(346.00, 267.00),
+                 new xy(369.00, 269.00), //
 
-            new xy(492.00, 475.00),
-            new xy(535.00, 461.00),
-            new xy(583.00, 461.00),
-            new xy(536.00, 412.00),
-            new xy(584.00, 411.00), //
-            new xy(397.00, 416.00),
-            new xy(396.00, 393.00),
-            new xy(397.00, 368.00),
-            new xy(397.00, 345.00),
-            new xy(397.00, 323.00),
-            new xy(397.00, 301.00),
+                 new xy(492.00, 475.00),
+                 new xy(535.00, 461.00),
+                 new xy(583.00, 461.00),
+                 new xy(536.00, 412.00),
+                 new xy(584.00, 411.00), //
+                 new xy(397.00, 416.00),
+                 new xy(396.00, 393.00),
+                 new xy(397.00, 368.00),
+                 new xy(397.00, 345.00),
+                 new xy(397.00, 323.00),
+                 new xy(397.00, 301.00),
 
-            new xy(587.00, 164.00),
-            new xy(535.00, 122.00),
-            new xy(584.00, 120.00),
-            new xy(536.00, 74.00),
-            new xy(585.00, 72.00),
-            new xy(535.00, 267.00), //
-            new xy(512.00, 268.00),
-            new xy(492.00, 268.00),
-            new xy(468.00, 267.00),
-            new xy(446.00, 268.00),
-            new xy(425.00, 266.00),
+                 new xy(587.00, 164.00),
+                 new xy(535.00, 122.00),
+                 new xy(584.00, 120.00),
+                 new xy(536.00, 74.00),
+                 new xy(585.00, 72.00),
+                 new xy(535.00, 267.00), //
+                 new xy(512.00, 268.00),
+                 new xy(492.00, 268.00),
+                 new xy(468.00, 267.00),
+                 new xy(446.00, 268.00),
+                 new xy(425.00, 266.00),
 
-            new xy(300.00, 63.00),
-            new xy(211.00, 123.00),
-            new xy(259.00, 121.00),
-            new xy(211.00, 74.00),
-            new xy(260.00, 74.00),
-            new xy(397.00, 119.00), //
-            new xy(397.00, 141.00),
-            new xy(397.00, 166.00),
-            new xy(396.00, 187.00),
-            new xy(397.00, 210.00),
-            new xy(396.00, 236.00)
-        };
+                 new xy(300.00, 63.00),
+                 new xy(211.00, 123.00),
+                 new xy(259.00, 121.00),
+                 new xy(211.00, 74.00),
+                 new xy(260.00, 74.00),
+                 new xy(397.00, 119.00), //
+                 new xy(397.00, 141.00),
+                 new xy(397.00, 166.00),
+                 new xy(396.00, 187.00),
+                 new xy(397.00, 210.00),
+                 new xy(396.00, 236.00)
+             }; 
         list_coordinates.AddRange(batch);
         base_coordinates.AddRange(group);
         Color_Location yellow = new Color_Location(0, base_coordinates);
         Color_Location green = new Color_Location(11, base_coordinates);
         Color_Location blue = new Color_Location(22, base_coordinates);
         Color_Location red = new Color_Location(33, base_coordinates);
+
+        */
         mainPathsOriginal = new List<Vector3> {
 
         new Vector3(3.425f, 0f, 0f),
@@ -454,24 +456,43 @@ public class Coordinate : MonoBehaviour
     };
         paths["YellowBase"] = new List<Vector3>
         {
-        new Vector3(211, 461, 0),
-        new Vector3(260, 459, 0),
-        new Vector3(210, 412, 0),
-        new Vector3(260, 413, 0)  // Add your path coordinates
+        new Vector3(-2.59f,3.63f, 0f),
+        new Vector3(-2.59f, 2.73f, 0f),
+        new Vector3(-3.5f, 2.73f, 0f),
+        new Vector3(-3.5f, 3.63f, 0f),  // Add your path coordinates
+        new Vector3(-3.7f, 1.9f, 0f)
+
+        // new Vector3(211, 461, 0),
+        // new Vector3(260, 459, 0),
+        // new Vector3(210, 412, 0),
+        // new Vector3(260, 413, 0)  // Add your path coordinates
     };
         paths["BlueBase"] = new List<Vector3>
         {
-        new Vector3(535, 461, 0),
-        new Vector3(583, 461, 0),
-        new Vector3(536, 412, 0),
-        new Vector3(584, 411, 0)  // Add your path coordinates
+                    new Vector3(-2.59f, -3.63f, 0f),
+        new Vector3(-2.59f, -2.73f, 0f),
+        new Vector3(-3.5f, -2.73f, 0f),
+        new Vector3(-3.5f, -3.63f, 0f),  // Add your path coordinates
+        new Vector3(-3.7f, -1.9f, 0f)
+
+
+        // new Vector3(535, 461, 0),
+        // new Vector3(583, 461, 0),
+        // new Vector3(536, 412, 0),
+        // new Vector3(584, 411, 0)  // Add your path coordinates
     };
         paths["GreenBase"] = new List<Vector3>
         {
-        new Vector3(535, 122, 0),
-        new Vector3(584, 120, 0),
-        new Vector3(536, 74, 0),
-        new Vector3(585, 72, 0)  // Add your path coordinates
+                    new Vector3(2.59f, 3.63f, 0f),
+        new Vector3(2.59f, 2.73f, 0f),
+        new Vector3(3.5f, 2.73f, 0f),
+        new Vector3(3.5f, 3.63f, 0f),  // Add your path coordinates
+        new Vector3(3.7f, 1.9f, 0f)
+    
+        // new Vector3(535, 122, 0),
+        // new Vector3(584, 120, 0),
+        // new Vector3(536, 74, 0),
+        // new Vector3(585, 72, 0)  // Add your path coordinates
     };
 
         // first coordinates are the first step that the planes take before going on the color board
